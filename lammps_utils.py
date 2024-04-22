d628885eae103c7ae75b094bfde395e432401610
Kee Myoung Nam <kmnam@Kees-MBP.stdusr.yale.internal>
2024-04-22 11:55:56 -0400

    Added simulation parsing

--- lammps_utils.py
+++ lammps_utils.py
@@ -3,9 +3,10 @@ Authors:
     Kee-Myoung Nam
 
 Last updated:
-    4/13/2024
+    4/22/2024
 """
 import numpy as np
+from scipy.sparse import csr_matrix
 
 #########################################################################
 def write_box_dims(xmin, xmax, ymin, ymax, zmin, zmax):
@@ -96,7 +97,10 @@ def write_molecule_coords(polymers, crosslinkers, rng, polymer_type=1,
             atom_id += 1
 
     # ... then write the crosslinker coordinates
-    is_tetrahedral = (crosslinkers[0].coords.shape[0] == 5)
+    try:
+        is_tetrahedral = (crosslinkers[0].coords.shape[0] == 5)
+    except IndexError:   # If there are no crosslinkers, an exception is raised
+        pass
     for i, crosslinker in enumerate(crosslinkers):
         molecule_id = len(polymers) + i + 1
         # If crosslinkers are tetrahedral, choose a peripheral atom to be sticky
@@ -170,4 +174,242 @@ def write_molecule_bonds(polymers, crosslinkers, polymer_bond_type=1,
         first_atom_id += crosslinker.coords.shape[0]
 
     return outstr
+
+#########################################################################
+class Simulation:
+    """
+    A basic wrapper for a LAMMPS simulation.
+    """
+    def __init__(self, n_atoms, atom_types, atom_masses, molecule_ids, 
+                 xmin, xmax, ymin, ymax, zmin, zmax):
+        """
+        Initialize a Simulation object. 
+        """
+        # Number of atoms in the simulation
+        self.n_atoms = n_atoms
+
+        # Array of atom type IDs for each atom 
+        self.atom_types = atom_types
+
+        # Dict of atom masses corresponding to each atom type 
+        self.atom_masses = atom_masses
+
+        # Array of molecule IDs for each atom 
+        self.molecule_ids = molecule_ids
+
+        # Simulation domain bounds 
+        self.xlim = (xmin, xmax)
+        self.ylim = (ymin, ymax)
+        self.zlim = (zmin, zmax)
+
+        # Lists of atomic configurations, bonding configurations, and their
+        # corresponding timepoints
+        self.n_frames = 0
+        self.times = []
+        self.coords = []    # List of 3-column arrays
+        self.bonds = []     # List of sparse matrices 
+
+    #####################################################################
+    def append_frame(self, time, coords, bonds):
+        """
+        Append a frame to the Simulation object.
+        """
+        self.times.append(time)
+        self.coords.append(coords)
+        self.bonds.append(bonds)
+
+#########################################################################
+def parse_init(init_filename):
+    """
+    Parse the initial configuration file for a simulation. 
+    """
+    # Parse the initial configuration file ... 
+    with open(init_filename) as f:
+        n_read = 0
+
+        # Line 3 contains the number of atoms 
+        while n_read < 2:
+            f.readline()
+            n_read += 1
+        line = f.readline()
+        n_atoms = int(line.split()[0])
+        n_read += 1
+
+        # Line 4 contains the number of bonds
+        line = f.readline()
+        n_bonds = int(line.split()[0])
+        n_read += 1
+
+        # Line 9 contains the number of atom types 
+        while n_read < 8:
+            f.readline()
+            n_read += 1
+        line = f.readline()
+        n_atom_types = int(line.split()[0])
+        n_read += 1
+
+        # Lines 15, 16, 17 contain the simulation domain bounds 
+        while n_read < 14:
+            f.readline()
+            n_read += 1
+        split = f.readline().split()
+        xmin, xmax = float(split[0]), float(split[1])
+        split = f.readline().split()
+        ymin, ymax = float(split[0]), float(split[1])
+        split = f.readline().split()
+        zmin, zmax = float(split[0]), float(split[1])
+        n_read += 3
+
+        # The next set of lines contain the atom masses 
+        f.readline()
+        f.readline()    # "Masses"
+        f.readline()
+        n_read += 3
+        atom_masses = {}
+        for i in range(n_atom_types):
+            split = f.readline().split()
+            atom_masses[int(split[0])] = float(split[1])
+            n_read += 1
+
+        # The next set of lines contain the atom types, molecule IDs, and
+        # corresponding coordinates
+        f.readline()
+        f.readline()    # "Atoms"
+        f.readline()
+        n_read += 3
+        atom_types = []
+        molecule_ids = []
+        coords = np.zeros((n_atoms, 3))
+        for i in range(n_atoms):
+            split = f.readline().split()
+            molecule_ids.append(int(split[1]))
+            atom_types.append(int(split[2]))
+            coords[i, 0] = float(split[3])
+            coords[i, 1] = float(split[4])
+            coords[i, 2] = float(split[5])
+            n_read += 1
+
+        # The next set of lines contain the bonds 
+        f.readline()
+        f.readline()    # "Bonds"
+        f.readline()
+        n_read += 3
+        bond_i = []
+        bond_j = []
+        bond_types = []
+        for i in range(n_bonds):
+            split = f.readline().split()
+            bond_i.append(int(split[2]) - 1)
+            bond_j.append(int(split[3]) - 1)
+            bond_types.append(int(split[1]))
+        bonds = csr_matrix((bond_types, (bond_i, bond_j)), shape=(n_atoms, n_atoms))
+
+    # Instantiate a new Simulation object
+    sim = Simulation(
+        n_atoms, atom_types, atom_masses, molecule_ids, xmin, xmax, ymin,
+        ymax, zmin, zmax
+    )
+    sim.append_frame(0.0, coords, bonds)
+    
+    return sim
+
+#########################################################################
+def parse_simulation(init_filename, lammpstrj_filename, bond_filename):
+    """
+    Parse a simulation, given the initial configuration file, the .lammpstrj
+    file, and the .bond file.
+    """
+    # First initialize the simulation
+    sim = parse_init(init_filename)
+
+    # Each block in the .lammpstrj file has the format: 
+    #
+    # ITEM: TIMESTEP
+    # [time]
+    # ITEM: NUMBER OF ATOMS
+    # [number of atoms]
+    # ITEM: BOX BOUNDS pp pp pp
+    # [xmin] [xmax]
+    # [ymin] [ymax]
+    # [zmin] [zmax]
+    # ITEM: ATOMS id mol type xu yu zu
+    # 1 [molecule id] [atom type] [x-pos] [y-pos] [z-pos] [x-vel] [y-vel] [z-vel]
+    # 2 [molecule id] [atom type] [x-pos] [y-pos] [z-pos] [x-vel] [y-vel] [z-vel]
+    # ...
+    times = []
+    coords = []
+    with open(lammpstrj_filename) as f:
+        curr_block = []
+        curr_block.append(f.readline().strip())
+        for line in f:
+            # If a new block is encountered, process the old block
+            if line == 'ITEM: TIMESTEP\n':
+                # The timestep is in the second line 
+                timestep = int(curr_block[1])
+                # Skip over the number of atoms and the simulation domain
+                # boundaries, then parse the atom coords from the remaining
+                # lines 
+                curr_coords = []
+                for i in range(9, len(curr_block)):
+                    split = curr_block[i].split()
+                    curr_coords.append([
+                        float(split[3]), float(split[4]), float(split[5])
+                    ])
+                times.append(timestep)
+                coords.append(np.array(curr_coords))
+                curr_block = []
+            curr_block.append(line.strip())
+
+    # Each block in the .bond file has the format: 
+    #
+    # ITEM: TIMESTEP
+    # [time]
+    # ITEM: NUMBER OF ENTRIES
+    # [number of bonds]
+    # ITEM: BOX BOUNDS pp pp pp
+    # [xmin] [xmax]
+    # [ymin] [ymax]
+    # [zmin] [zmax]
+    # ITEM: ENTRIES index c_1[1] c_1[2] c_1[3]
+    # [bond id] [bond type] [atom i] [atom j]
+    # [bond id] [bond type] [atom i] [atom j]
+    # ...
+    bonds = []
+    with open(bond_filename) as f:
+        curr_block = []
+        curr_block.append(f.readline().strip())
+        for line in f:
+            # If a new block is encountered, process the old block
+            if line == 'ITEM: TIMESTEP\n':
+                # The timestep is in the second line 
+                timestep = int(curr_block[1])
+                # Skip over the timestep, number of bonds and the simulation
+                # domain boundaries, then parse the bonds from the remaining
+                # lines
+                bond_i = []
+                bond_j = []
+                bond_types = []
+                for i in range(9, len(curr_block)):
+                    split = curr_block[i].split()
+                    bond_i.append(int(split[2]) - 1)
+                    bond_j.append(int(split[3]) - 1)
+                    bond_types.append(int(split[1]))
+                bonds.append(
+                    csr_matrix(
+                        (bond_types, (bond_i, bond_j)),
+                        shape=(sim.n_atoms, sim.n_atoms)
+                    )
+                )
+                curr_block = []
+            curr_block.append(line.strip())
+
+    # Replace the initial configuration
+    sim.coords[0] = coords[0]
+    sim.bonds[0] = bonds[0]
+
+    # Append each subsequent configuration
+    for i in range(1, len(times)):
+        sim.append_frame(times[i], coords[i], bonds[i])
+
+    return sim
 
