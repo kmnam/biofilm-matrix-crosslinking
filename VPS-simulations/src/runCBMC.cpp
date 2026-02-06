c31e476fcb38e78cd50b174e71343d3957dbc798
Kee-Myoung Nam <kmnam.99.12@gmail.com>
2026-02-05 19:43:05 -0500

    First commit

---
+++ VPS-simulations/src/runCBMC.cpp
@@ -0,0 +1,185 @@
+/**
+ * Authors:
+ *     Kee-Myoung Nam
+ *
+ * Last updated:
+ *     2/5/2026
+ */
+
+#include <iostream>
+#include <fstream>
+#include <Eigen/Dense>
+#include "../include/utils.hpp"
+#include "../include/polymerConfiguration.hpp"
+#include "../include/cbmc.hpp"
+
+int main(int argc, char** argv)
+{
+    // Parse input json file 
+    std::string json_filename = argv[1];
+    boost::json::object json_data = parseConfigFile(json_filename).as_object();
+
+    // Define potential and sampling parameters 
+    Matrix<double, 3, 1> r0 = Matrix<double, 3, 1>::Zero();
+    std::unordered_map<std::string, double> lj_params, 
+                                            fene_params, 
+                                            angle_params, 
+                                            dihedral_params,
+                                            internal_move_params;
+    const double kT = 1.380649e-2 * 300;    // Use "nano" units 
+    const int length = json_data["length"].as_int64(); 
+    lj_params["eps"] = json_data["lj_eps"].as_double(); 
+    lj_params["sigma"] = json_data["lj_sigma"].as_double(); 
+    fene_params["K"] = json_data["fene_K_by_kT"].as_double() * kT;
+    fene_params["R0"] = json_data["fene_R0"].as_double();
+    AngleMode angle_mode = (
+        json_data["angle_mode"] == 0 ? AngleMode::COSINE : AngleMode::GAUSSIAN
+    ); 
+    if (angle_mode == AngleMode::COSINE)
+    { 
+        angle_params["K"] = json_data["cosine_K_by_kT"].as_double() * kT; 
+        angle_params["theta0"] = (
+            json_data["cosine_theta0"].as_double() * boost::math::constants::pi<double>() / 180
+        );
+    }
+    else 
+    {
+        angle_params["A1"] = json_data["gaussian_A1"].as_double(); 
+        angle_params["A2"] = json_data["gaussian_A2"].as_double(); 
+        angle_params["w1"] = json_data["gaussian_w1"].as_double(); 
+        angle_params["w2"] = json_data["gaussian_w2"].as_double(); 
+        angle_params["theta1"] = (
+            json_data["gaussian_theta1"].as_double() * boost::math::constants::pi<double>() / 180
+        ); 
+        angle_params["theta2"] = (
+            json_data["gaussian_theta2"].as_double() * boost::math::constants::pi<double>() / 180
+        );
+    } 
+    dihedral_params["K"] = json_data["dihedral_K_by_kT"].as_double() * kT;
+    const double collision_threshold = json_data["init_collision_threshold"].as_double(); 
+    const int max_tries_per_atom = json_data["init_max_tries_per_atom"].as_int64();
+    const int max_n_backtracks = json_data["init_max_n_backtracks"].as_int64();
+    const int n_candidates = json_data["n_candidates"].as_int64(); 
+    const int n_target = json_data["n_target_configs"].as_int64(); 
+    const int n_burnin = json_data["n_burnin"].as_int64(); 
+    const int mod_collect = json_data["collect_config_every_iter"].as_int64();
+    const int mod_write = json_data["write_configs_every_iter"].as_int64();  
+    const int max_iter = n_burnin + (n_target - 1) * mod_collect; 
+    const int max_stall = json_data["max_stall_iter"].as_int64();
+
+    // Fix move probabilities 
+    Matrix<double, 3, 1> move_probs; 
+    move_probs << json_data["reptation_prob"].as_double(), 
+                  json_data["terminal_segment_move_prob"].as_double(), 
+                  json_data["internal_segment_move_prob"].as_double();  
+
+    // Parse terminal and internal segment lengths
+    const int terminal_segment_length = json_data["terminal_segment_length"].as_int64(); 
+    const int internal_segment_length = json_data["internal_segment_length"].as_int64();
+
+    // Parse internal move parameters (with default values as follows) 
+    try
+    {
+        internal_move_params["init_tangent_stepsize"]
+            = json_data["internal_init_tangent_stepsize"].as_double();
+    }
+    catch (boost::wrapexcept<boost::system::system_error>& e)
+    {
+        internal_move_params["init_tangent_stepsize"] = 0.1 * lj_params["sigma"]; 
+    } 
+    try
+    {
+        internal_move_params["min_tangent_stepsize"]
+            = json_data["internal_min_tangent_stepsize"].as_double();
+    }
+    catch (boost::wrapexcept<boost::system::system_error>& e)
+    {
+        internal_move_params["init_tangent_stepsize"] = 0.01 * lj_params["sigma"]; 
+    }
+    try
+    { 
+        internal_move_params["dx"] = json_data["dx"].as_double(); 
+    }
+    catch (boost::wrapexcept<boost::system::system_error>& e)
+    {
+        internal_move_params["dx"] = 1e-8; 
+    }
+    try
+    {
+        internal_move_params["newton_tol"] = json_data["newton_tol"].as_double(); 
+    }
+    catch (boost::wrapexcept<boost::system::system_error>& e)
+    {
+        internal_move_params["newton_tol"] = 1e-5;
+    }
+    try
+    {
+        internal_move_params["min_newton_stepsize"]
+            = json_data["min_newton_stepsize"].as_double(); 
+    }
+    catch (boost::wrapexcept<boost::system::system_error>& e)
+    {
+        internal_move_params["min_newton_stepsize"] = 1e-4; 
+    }
+    try
+    {
+        internal_move_params["armijo_const"] = json_data["armijo_const"].as_double(); 
+    }
+    catch (boost::wrapexcept<boost::system::system_error>& e)
+    {
+        internal_move_params["armijo_const"] = 1e-4;
+    }
+
+    // Initialize random number generator 
+    const int seed = std::stoi(argv[3]);
+    boost::random::mt19937 rng(seed); 
+    boost::random::uniform_01<> uniform_dist;  
+
+    // Generate an initial configuration
+    PolymerConfiguration<double> config = generateKMer<double>(
+        length, lj_params, fene_params, angle_mode, angle_params,
+        dihedral_params, r0, collision_threshold, max_tries_per_atom, 
+        max_n_backtracks, rng, uniform_dist, Units::NANO, 300
+    );
+
+    // Write the initial coordinates to file 
+    std::string outfilename = argv[2]; 
+    std::ofstream outfile(outfilename);
+    outfile << std::setprecision(10); 
+    outfile << "CONFIG\tINIT\n";
+    Matrix<double, Dynamic, 3> init_coords = config.getSegment(0, length);  
+    for (int i = 0; i < length; ++i)
+    {
+        outfile << init_coords(i, 0) << '\t'
+                << init_coords(i, 1) << '\t'
+                << init_coords(i, 2) << std::endl; 
+    }
+
+    // Initialize and run CBMC sampler 
+    const double neighbor_threshold = 1.1 * pow(2, 1. / 6.) * lj_params["sigma"]; 
+    PolymerCBMCSampler<double> sampler(
+        config, lj_params, neighbor_threshold, fene_params, angle_mode, 
+        angle_params, dihedral_params, rng
+    ); 
+    Matrix<double, Dynamic, Dynamic> ensemble_coords = sampler.run(
+        n_candidates, internal_move_params, move_probs, terminal_segment_length,
+        internal_segment_length, max_iter, n_burnin, mod_collect, mod_write,
+        max_stall, outfile, true
+    ); 
+    const int n_ensemble = ensemble_coords.rows();  
+
+    // Calculate the persistence length and write to file
+    PolymerEnsemble<double> ensemble; 
+    for (int i = 0; i < n_ensemble; ++i)
+    {
+        Matrix<double, Dynamic, 3> coords_i(length, 3); 
+        for (int j = 0; j < length; ++j)
+            coords_i.row(j) = ensemble_coords(i, Eigen::seqN(3 * j, 3)); 
+        PolymerConfiguration<double> config(coords_i, Units::NANO, 300);
+        ensemble.push_back(config);  
+    } 
+    double persist_length = getPersistenceLength<double>(ensemble);
+    outfile << "## PERSISTENCE_LENGTH\t" << persist_length << std::endl;  
+
+    return 0; 
+}
