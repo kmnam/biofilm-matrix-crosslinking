a63369df5939f6a9804bff34c4849584b4a5b725
Kee-Myoung Nam <kmnam.99.12@gmail.com>
2026-05-28 13:13:30 -0400

    Integrated lammpstrjToLammpsData

--- VPS-simulations/src/convert.cpp
+++ VPS-simulations/src/convert.cpp
@@ -5,7 +5,7 @@
  *     Kee-Myoung Nam
  *
  * Last updated:
- *     4/11/2026
+ *     5/28/2026
  */
 
 #include <fstream>
@@ -16,40 +16,52 @@
 #include "../include/polymerMelt.hpp"
 
 /**
- * Write a configurations file from a .lammpstrj file. 
+ * Write a LAMMPS initial data file from the final configuration in a 
+ * .lammpstrj file.
  *
- * Each configuration in the input .lammpstrj file is written as a
- * configuration in the output configurations file, in the same order.
+ * Other required parameters are taken from the given JSON file. 
  *
- * Since the .lammpstrj file will not contain information that belongs in the
- * header in the configurations file, this function requires a JSON file
- * containing this information.  
- *
- * @param json_filename Path to JSON file containing header information. 
- * @param lammpstrj_filename Path to input .lammpstrj file. 
- * @param outfilename Path to output file. 
+ * @param lammpstrj_filename Path to input .lammpstrj file.
+ * @param json_filename Path to input JSON file.  
+ * @param outfilename Path to output file.  
  */
-void lammpstrjToConfig(const std::string& json_filename, 
-                       const std::string& lammpstrj_filename, 
-                       const std::string& outfilename)
+void lammpstrjToLammpsData(const std::string& lammpstrj_filename,
+                           const std::string& json_filename, 
+                           const std::string& outfilename)
 {
-    // Parse the given .json and .lammpstrj files
+    // Parse input JSON file 
     boost::json::object json_data = parseConfigFile(json_filename).as_object();
-    double dt = json_data["dt"].as_double(); 
-    auto result = parseLammpstrj<double>(lammpstrj_filename, dt, Units::NANO, 300.0);
-    PolymerEnsemble<double> ensemble = result.first; 
-    std::vector<double> times = result.second;
+    double dt = json_data["dt"].as_double();
+    double xmax = json_data["domain_xmax"].as_double(); 
+    double xmin = -xmax; 
+    double ymax = json_data["domain_ymax"].as_double(); 
+    double ymin = -ymax; 
+    double zmax = json_data["domain_zmax"].as_double(); 
+    double zmin = -zmax; 
+    double mass = json_data["monomer_mass"].as_double();
 
-    // Collect potential parameters for energy calculations 
-    const double kT = 1.380649e-2 * 300.0;  
+    // Parse the .lammpstrj file  
+    auto result = parseMeltLammpstrj<double>(lammpstrj_filename, dt, Units::NANO, 300.0);
+    auto ensemble = result.first; 
+    std::vector<double> times = result.second;
+    
+    // Extract only the final configuration
+    PolymerMeltConfiguration<double> final_config = ensemble[ensemble.size() - 1]; 
+
+    // Collect potential parameters
     std::unordered_map<std::string, double> lj_params, 
                                             fene_params, 
                                             angle_params,
                                             dihedral_params; 
+    const double kT = 1.380649e-2 * 300;    // Use "nano" units
+    
+    // Parse Lennard-Jones and FENE potential parameters
     lj_params["eps"] = json_data["lj_eps"].as_double() * kT;
     lj_params["sigma"] = json_data["lj_sigma"].as_double(); 
     fene_params["K"] = json_data["fene_K"].as_double() * kT; 
     fene_params["R0"] = json_data["fene_R0"].as_double();
+
+    // Parse angle potential parameters
     AngleMode angle_mode = (
         json_data["angle_mode"].as_int64() == 0 ? AngleMode::COSINE : AngleMode::GAUSSIAN
     );
@@ -72,18 +84,28 @@ void lammpstrjToConfig(const std::string& json_filename,
         angle_params["theta2"] = (
             json_data["gaussian_theta2"].as_double() * boost::math::constants::pi<double>() / 180
         );
-    } 
+    }
+
+    // Parse dihedral potential parameters
     dihedral_params["K"] = json_data["dihedral_K"].as_double() * kT;
     dihedral_params["d"] = 1; 
     dihedral_params["n"] = 1;
+    try
+    {
+        dihedral_params["delta"] = (
+            json_data["dihedral_delta"].as_double() * boost::math::constants::pi<double>() / 180
+        ); 
+    }
+    catch (boost::wrapexcept<boost::system::system_error>& e) { }
 
-    // Write the configurations to an output file
-    std::unordered_map<std::string, double> params;    // Leave empty 
-    writeConfigFile<double>(
-        ensemble, params, lj_params, fene_params, angle_mode, angle_params,
-        dihedral_params, outfilename
+    // Write the final configuration and potential parameters to an output
+    // file
+    std::string header = ""; 
+    final_config.writeLammps(
+        outfilename, lj_params, fene_params, angle_mode, angle_params,
+        dihedral_params, header, mass
     );  
-}
+} 
 
 /**
  * Write a .lammpstrj file from a configurations file.
@@ -141,10 +163,10 @@ int main(int argc, char** argv)
     // Run the desired conversion
     if (mode == 0)
     {
-        const std::string json_filename = argv[2]; 
-        const std::string infilename = argv[3]; 
+        const std::string lammpstrj_filename = argv[2];
+        const std::string json_filename = argv[3];
         const std::string outfilename = argv[4]; 
-        lammpstrjToConfig(json_filename, infilename, outfilename);
+        lammpstrjToLammpsData(lammpstrj_filename, json_filename, outfilename); 
     }
     else if (mode == 1)
     {
